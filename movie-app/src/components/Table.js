685963cc03da2330c3af46906ce2fccea28e7f8c
maridi manoj kumar <21a91a6152@aec.edu.in>
2024-12-02 23:03:58 +0530

    updatedtabledata
    tabledataadded

--- movie-app/src/components/Table.js
+++ movie-app/src/components/Table.js
@@ -2,6 +2,18 @@ const Table = () => {
     return(
         <div>
             <h1>Table</h1>
+            <table>
+                <thead>
+                    <td>home</td>
+                    <td>group</td>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>123</td>
+                        <td>2345</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
